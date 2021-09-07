5aaeb715fca7f31f44eb9456fc1a9464e220a2e7
Mahdi <Mahdikhodadadi12x@gmail.com>
2021-09-07 23:07:14 +0430

    Add JavaScript Example

---
+++ js/megajs.js
@@ -0,0 +1,19 @@
+let Name = "Mahdi Khodadadi";
+let output;
+
+// output = Name.concat(" Khodadadi")
+
+output = Name.toLocaleLowerCase();
+// output = Name.toUpperCase();
+// // output = Name.includes("");
+// output = Name.indexOf("i");
+// output = Name.length;
+// // output = Name.concat(" ", "Khodadadi");
+// output = Name.substring(2, 8);
+// output = Name.repeat(3)
+
+// output = Name.split("h")
+
+output = Name.replace("Mahdi", "Hadis")
+
+console.log(output);
