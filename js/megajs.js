5a891429e729df1217f954d1037de2104b087075
Mahdi <Mahdikhodadadi12x@gmail.com>
2021-09-08 21:14:18 +0430

    Updating JS

--- js/megajs.js
+++ js/megajs.js
@@ -1,19 +1,29 @@
-let Name = "Mahdi Khodadadi";
+// let Name = "Mahdi Khodadadi";
+// let output;
+
+// // output = Name.concat(" Khodadadi")
+
+// output = Name.toLocaleLowerCase();
+// // output = Name.toUpperCase();
+// // // output = Name.includes("");
+// // output = Name.indexOf("i");
+// // output = Name.length;
+// // // output = Name.concat(" ", "Khodadadi");
+// // output = Name.substring(2, 8);
+// // output = Name.repeat(3)
+// // output = Name.replace("Mahdi", "Hadis")
+// // output = Name.split("h")
+
+// output = Name.replace("Mahdi", "Hadis")
+
+let number = 15;
+
+let number2 = 20;
+
 let output;
 
-// output = Name.concat(" Khodadadi")
 
-output = Name.toLocaleLowerCase();
-// output = Name.toUpperCase();
-// // output = Name.includes("");
-// output = Name.indexOf("i");
-// output = Name.length;
-// // output = Name.concat(" ", "Khodadadi");
-// output = Name.substring(2, 8);
-// output = Name.repeat(3)
+output = Math.PI
 
-// output = Name.split("h")
-
-output = Name.replace("Mahdi", "Hadis")
 
 console.log(output);
