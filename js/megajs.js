18948e164860c50bd1ece41897f46267ad77be2f
Mahdi <Mahdikhodadadi12x@gmail.com>
2021-09-10 21:04:53 +0430

    Updating JS

--- js/megajs.js
+++ js/megajs.js
@@ -16,14 +16,108 @@
 
 // output = Name.replace("Mahdi", "Hadis")
 
-let number = 15;
+// let number = 15;
 
-let number2 = 20;
+// // let number2 = 20;
 
-let output;
-
-
-output = Math.PI
+// let output;
 
 
-console.log(output);
+// output = Math.PI
+
+
+// console.log(output);
+
+
+
+
+// let array;
+
+// array = [1, 4, 3, 1, 5, 2, 3]
+
+// array = [14, "Mahdi", 11.212, true]
+// array = new Array(22, 31, 44, "11")
+let name = "Mahdi";
+
+let s = `
+<h3>
+Hello ${name}<br><br>
+ this is Testing JavaScrip
+</h3>
+
+`;
+
+let app = document.getElementById("app");
+app.innerHTML = s
+
+
+
+let array = [22, 11, 41, 422, 14, 1]
+
+let log;
+
+array[2] = 5
+array[5] = 2
+    // log = Array.isArray(array)
+
+// log = array.length
+
+// log = array.indexOf(2)
+
+// log = array.shift()
+// log = array.unshift(1)
+// log = array.splice(2, 0, 2, 12, 22)
+// log = array.pop(1)
+// log = array.push(22, 32, 11)
+
+
+// console.log(log);
+// console.log(array);
+// console.log(array);
+
+
+
+// function sum(a, b) {
+
+//     if (a == 0 || b == 0) {
+
+//         console.log("Put The Number ");
+//     } else {
+//         return a + b
+//     }
+
+//     return "Lotfan Addad Vared Konid"
+
+// }
+
+
+// console.log(sum(4, 4));
+
+//     (function Hello(name) {
+
+//     console.log("Hello " + name);
+// }("Mahdi"))
+
+// let person = {
+//     name: "mahdi",
+//     email: "mahdikhodadadi12x@gmail.com",
+//     age: 21,
+
+//     info: function() {
+
+//         return "userName Has: " + this.name + "  And Age Are :" + this.age
+
+//     }
+
+// }
+// console.log(person);
+// console.log(person.info());
+
+
+// let time = new Date();
+
+// let out = time.getMilliseconds()
+// let out = time.getDay()
+// let out = time.toDateString()
+// let out = time.setDate(20)
+// console.log(time);
